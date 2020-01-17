0fa5e8e743b2052bee7673459ce45fc5a27436c1
Erik Rodriguez <eric19.21.03@gmail.com>
2020-01-17 11:03:58 -0800

    trying to make card component work properly

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    console.log(response);
+    const cardEntry = document.querySelector(".cards-containe");
+    Object.entries(response.data.articles).forEach((item) => {
+      cardEntry.append(cardComponent(item));
+    });
+  })
+  .catch((error) => {
+    console.log(`This is a http error: ${error}`);
+  });
+
+function cardComponent(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let a = 0; a < array.length; a++) {
+      const outerCard = document.createElement("div");
+      const cardHeader = document.createElement("div");
+      const cardAuthor = document.createElement("div");
+      const cardImgContainer = document.createElement("div");
+      const cardImage = document.createElement("img");
+      const cardSpan = document.createElement("span");
+
+      outerCard.classList = "card";
+      cardHeader.classList = "headline";
+      cardAuthor.classList = "author";
+      cardImgContainer.classList = "img-container";
+
+      cardImage.src = array[i][1][a].authorPhoto;
+
+      cardHeader.textContent = array[i][1][a].headline;
+
+      cardSpan.textContent = `By ${array[i][1][a].authorName}`;
+
+      outerCard.append(cardHeader);
+      outerCard.append(cardAuthor);
+      cardAuthor.append(cardImgContainer);
+      cardImgContainer.append(cardImage);
+      cardAuthor.append(cardSpan);
+    }
+  }
+
+  return outerCard;
+}
